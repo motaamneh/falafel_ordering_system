6dd0b4bfb1fef1e9935649077e1c7b607d03ba9c
Mohammad AlTaamneh <mohammad.altaamneh@adres.ae>
2026-02-04 13:20:27 +0300

    first commit

---
+++ src/main/java/com/motaamneh/falafel/FalafelOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.motaamneh.falafel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalafelOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalafelOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motaamneh/falafel/FalafelOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motaamneh.falafel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalafelOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
