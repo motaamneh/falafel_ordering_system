190f29dc55c547f8fc16eb6b3cd450a75ee70818
Mohammad Taamneh <mtaamneh292@gmail.com>
2026-02-05 14:15:33 +0300

    test

--- src/main/java/com/motaamneh/falafel/entity/Order.java
+++ src/main/java/com/motaamneh/falafel/entity/Order.java
@@ -1,4 +1,11 @@
 package com.motaamneh.falafel.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
