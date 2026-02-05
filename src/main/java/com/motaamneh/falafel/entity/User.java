fa764f5f97e7df164c07d06a76745ceb01b0eb2a
Mohammad AlTaamneh <mohammad.altaamneh@adres.ae>
2026-02-05 12:52:49 +0300

    User Entity implemnted with the Enum roles for user

--- src/main/java/com/motaamneh/falafel/entity/User.java
+++ src/main/java/com/motaamneh/falafel/entity/User.java
@@ -1,4 +1,75 @@
 package com.motaamneh.falafel.entity;
 
+import com.motaamneh.falafel.model.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(unique = true, length = 100, nullable = false)
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private String email;
+
+
+    @Column(length = 250, nullable = false)
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8,message = "Password must be at least 8 characters")
+    private String password;
+
+
+    @Column(name = "full_name", length = 100, nullable = false)
+    @NotNull(message = "Full name cannot be null")
+    @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
+    private String fullName;
+
+
+    @Column(length = 20, nullable = false)
+    @NotNull(message = "Phone Number cannot be null")
+    private String phone;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    @NotNull(message = "Role cannot be null")
+    private Role role;
+
+
+    @Column(name = "is_enabled", nullable = false)
+    private Boolean isEnabled = true;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+
+
 }
---
+++ src/main/java/com/motaamneh/falafel/model/Role.java
@@ -0,0 +1,6 @@
+package com.motaamneh.falafel.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
