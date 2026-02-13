fdf26395e4748f8c1368f1fe6efd8b3f2140e6e8
Mohammad Taamneh <mtaamneh292@gmail.com>
2026-02-13 12:24:31 +0300

    User mapper implemented also learned a lot about mapping and dtos req and resp

--- src/main/java/com/motaamneh/falafel/mapper/RestaurantMapper.java
+++ src/main/java/com/motaamneh/falafel/mapper/RestaurantMapper.java
@@ -1,4 +1,10 @@
 package com.motaamneh.falafel.mapper;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RestaurantMapper {
+
+
+
 }
--- src/main/java/com/motaamneh/falafel/mapper/UserMapper.java
+++ src/main/java/com/motaamneh/falafel/mapper/UserMapper.java
@@ -1,7 +1,43 @@
 package com.motaamneh.falafel.mapper;
 
+import com.motaamneh.falafel.dto.request.UserRegistrationDto;
+import com.motaamneh.falafel.dto.response.LoginResponseDto;
+import com.motaamneh.falafel.dto.response.UserResponseDto;
+import com.motaamneh.falafel.entity.User;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UserMapper {
+
+    public UserResponseDto toResponseDto(User user) {
+        return new UserResponseDto(
+                user.getId(),
+                user.getEmail(),
+                user.getFullName(),
+                user.getPhone(),
+                user.getRole().name(),
+                user.getIsEnabled(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+
+    public LoginResponseDto toLoginResponseDto(User user){
+        return new LoginResponseDto(
+          user.getId(),
+                user.getEmail(),
+                user.getFullName(),
+                user.getRole().name(),
+                "Login successful"
+
+        );
+    }
+
+    public List<UserResponseDto> toResponseDto(List<User> users){
+        return users.stream().map(this::toResponseDto).toList();
+    }
+
+
 }
