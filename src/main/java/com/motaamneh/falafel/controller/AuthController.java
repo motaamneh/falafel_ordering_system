d42731e6b7124f00ce8ff38f3de1c5b045dc7dea
Mohammad Taamneh <mtaamneh292@gmail.com>
2026-02-17 14:17:59 +0300

    Created ADMIN MANUALLY and AuthController implemented

---
+++ src/main/java/com/motaamneh/falafel/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.motaamneh.falafel.controller;
+
+import com.motaamneh.falafel.dto.request.RestaurantLoginRequestDto;
+import com.motaamneh.falafel.dto.request.UserLoginRequestDto;
+import com.motaamneh.falafel.dto.request.UserRegistrationDto;
+import com.motaamneh.falafel.dto.response.LoginResponseDto;
+import com.motaamneh.falafel.dto.response.UserResponseDto;
+import com.motaamneh.falafel.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponseDto> signup(@RequestBody UserRegistrationDto dto) {
+        UserResponseDto responseDto = authService.registerUser(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @PostMapping("/login/user")
+    public ResponseEntity<LoginResponseDto> loginUser(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+        LoginResponseDto response = authService.loginUser(userLoginRequestDto);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @PostMapping("/login/restaurant")
+    public ResponseEntity<LoginResponseDto> loginRestaurant(@RequestBody RestaurantLoginRequestDto restaurantLoginRequestDto) {
+        LoginResponseDto response = authService.loginRestaurant(restaurantLoginRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
